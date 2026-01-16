21a7bfd1c9147bfefd946ca000a31cfa47c4afe8
Vergueirod <diego.ssv@outlook.com>
2026-01-16 16:30:49 -0300

    Palindrome validation

--- velid_palindrome.py
+++ velid_palindrome.py
@@ -1,4 +1,6 @@
 word_to_validate = ['m','a','d','a','m']
+#word_to_validate = ['d','i','e','g','o']
+
 len_array = 0
 
 for index, value in enumerate(word_to_validate):
@@ -8,3 +10,23 @@ for index, value in enumerate(word_to_validate):
 print(f'{len_array}')
 
 # One start in the end of the word, and another pointer start in the beginner of the word and I need compare boths in each cicle.
+
+invert_word_array = []
+
+for index in word_to_validate[::-1]:
+    invert_word_array.append(index)
+    print(index)
+
+print(invert_word_array)
+
+count = 0
+
+for i in range(0,len(word_to_validate),1):
+    if word_to_validate[i] != invert_word_array[i]:
+        print(f"This is not a palindrome.")
+        break
+    else:
+        count += 1
+
+if count == len(word_to_validate):
+    print("It is a palindrome!")
