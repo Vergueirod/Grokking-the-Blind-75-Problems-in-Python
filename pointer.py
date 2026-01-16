226de3d142af40e3448e2f666afe68dab18b952b
Vergueirod <diego.ssv@outlook.com>
2026-01-16 13:44:49 -0300

    Compare position in the loop

--- pointer.py
+++ pointer.py
@@ -1,36 +1,11 @@
-#a = 10
-#b = a
-#print(id(a))
-#print(id(b))
-'''
-lst1 = [10,20,30]
+list = [10,50,30]
 
-for x in lst1:
-    print(x, id(x))
+print(list)
 
-print(f"#####################")    
-
-
-lst2 = [10,20,30]
-
-for x in lst2:
-    x = x + 1
-
-print(lst2)
-'''
-
-print(f"#####################")    
-
-
-lst3 = [30,20,10]
-validation = 0
-new_lst = []
-
-'''
-for x in lst3:
-    print(f"{x}")
-print(lst3)
-'''
-
-for idx, value in enumerate(lst3):
-    print(idx, value, id(value))
+for idx, value in enumerate(list):
+    print(
+        "idx:", idx,
+        "value:", value,
+        "id(value):", id(value),
+        "id(list[idx]):", id(list[idx]),
+    )
