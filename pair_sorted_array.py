00406bbbb65276f40d44b071c3b80d46d9dfa3a3
Vergueirod <diego.ssv@outlook.com>
2026-01-17 10:00:58 -0300

    Solution use the two pointers

--- pair_sorted_array.py
+++ pair_sorted_array.py
@@ -23,6 +23,24 @@ class SortedArray:
             #print(f"{p1}")
             
         return print(f"{p1} and {self.array[j]}")
+    
+
+    def find_pair_with_sun(self):
+        left = 0
+        right = len(self.array) - 1
+
+        while left < right:
+            current_sum = self.array[left] + self.array[right]
+
+            if current_sum == self.t:
+                   return self.array[left], self.array[right]
+            elif current_sum < self.t:
+                   left += 1
+            else:
+                   right -= 1 
+        
+        return None
 
 enginee = SortedArray(sorted_array, pair_validation)
-enginee.validad_array()
+pair = enginee.find_pair_with_sun()
+print(pair)
